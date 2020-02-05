94d0b544a5a1975dfc609aadb9ddda45328a18a3
tuulingo <oliver.tuuling@tptlive.ee>
2020-02-05 14:57:31 +0200

    user data

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,22 @@ resource "aws_instance" "web" {
   ami           = "ami-07ebfd5b3428b6f4d"
   instance_type = "t2.micro"
 
+  key_name = "050220"
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Tsau maailm"
   }
+
+  user_data = file("${path.module}/ec2-setup.sh")
 }
+
+
+output "web_id" {
+  value = aws_instance.web.id
+}
+
+
+output "web_ip" {
+  value = aws_instance.web.public_ip
+}
+
